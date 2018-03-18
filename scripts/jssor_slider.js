67fc51550ffac0c1902c965306b6db9d6a29e61c
Daniel <vivadaniel@hotmail.com>
2018-03-18 21:47:46 +0100

    jssor top

--- scripts/jssor_slider.js
+++ scripts/jssor_slider.js
@@ -136,9 +136,9 @@ jssor_3_slider_init = function() {
             $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
             /*#endregion responsive code end*/
         };
-jssor_0_slider_init = function() {
+jssor_4_slider_init = function() {
 
-            var jssor_0_options = {
+            var jssor_4_options = {
               $AutoPlay: 1,
               $FillMode: 4,
               $PauseOnHover: 3,
@@ -150,21 +150,64 @@ jssor_0_slider_init = function() {
               }
             };
 
-            var jssor_0_slider = new $JssorSlider$("jssor_0", jssor_0_options);
+            var jssor_4_slider = new $JssorSlider$("jssor_1", jssor_4_options);
+
+            //make sure to clear margin of the slider container element
+            jssor_4_slider.$Elmt.style.margin = "";
 
             /*#region responsive code begin*/
 
+            /*
+                parameters to scale jssor slider to fill parent container
+
+                MAX_WIDTH
+                    prevent slider from scaling too wide
+                MAX_HEIGHT
+                    prevent slider from scaling too high, default value is original height
+                MAX_BLEEDING
+                    prevent slider from bleeding outside too much, default value is 1
+                    0: contain mode, allow up to 0% to bleed outside, the slider will be all inside parent container
+                    1: cover mode, allow up to 100% to bleed outside, the slider will cover full area of parent container
+                    0.1: flex mode, allow up to 10% to bleed outside, this is better way to make full window slider, especially for mobile devices
+            */
+
             var MAX_WIDTH = 3000;
+            var MAX_HEIGHT = 310;
+            var MAX_BLEEDING = 0.128;
 
             function ScaleSlider() {
-                var containerElement = jssor_0_slider.$Elmt.parentNode;
+                var containerElement = jssor_4_slider.$Elmt.parentNode;
                 var containerWidth = containerElement.clientWidth;
 
                 if (containerWidth) {
+                    var originalWidth = jssor_4_slider.$OriginalWidth();
+                    var originalHeight = jssor_4_slider.$OriginalHeight();
+
+                    var containerHeight = containerElement.clientHeight || originalHeight;
 
                     var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+                    var expectedHeight = Math.min(MAX_HEIGHT || containerHeight, containerHeight);
 
-                    jssor_0_slider.$ScaleWidth(expectedWidth);
+                    //constrain bullets, arrows inside slider area, it's optional, remove it if not necessary
+                    if (MAX_BLEEDING >= 0 && MAX_BLEEDING < 1) {
+                        var widthRatio = expectedWidth / originalWidth;
+                        var heightRatio = expectedHeight / originalHeight;
+                        var maxScaleRatio = Math.max(widthRatio, heightRatio);
+                        var minScaleRatio = Math.min(widthRatio, heightRatio);
+
+                        maxScaleRatio = Math.min(maxScaleRatio / minScaleRatio, 1 / (1 - MAX_BLEEDING)) * minScaleRatio;
+                        expectedWidth = Math.min(expectedWidth, originalWidth * maxScaleRatio);
+                        expectedHeight = Math.min(expectedHeight, originalHeight * maxScaleRatio);
+                    }
+
+                    //scale the slider to expected size
+                    jssor_4_slider.$ScaleSize(expectedWidth, expectedHeight, MAX_BLEEDING);
+
+                    //position slider at center in vertical orientation
+                    jssor_4_slider.$Elmt.style.top = ((containerHeight - expectedHeight) / 2) + "px";
+
+                    //position slider at center in horizontal orientation
+                    jssor_4_slider.$Elmt.style.left = ((containerWidth - expectedWidth) / 2) + "px";
                 }
                 else {
                     window.setTimeout(ScaleSlider, 30);
